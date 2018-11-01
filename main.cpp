63c7eaacfa556665dc38da7819aa9dd7ebd02e19
Maschamagin <35238192+Maschamagin@users.noreply.github.com>
2018-11-01 23:31:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,24 @@
 #include <fstream>
 #include <string>
 #include "images.h"
-#include <vector>
-#include <fstream>
 #include <iostream>
 using namespace std;
 
 int main()
     {
+    int row;
+    int column;
+    char new_value;
+    cout << "type in which coordinate you would like to acces(row, column): " << endl;
+    cin >> row >> column;
+
     images test;
     test.read_image();
+    test.operator()(row, column);
+    cout << "what should be the new value for the coordinate? " << endl;
+    cout << "please choose between 1 and 0. " << endl;
+    cin >> new_value;
+    test.operator()(row, column, new_value);
 
     return 0;
 }
